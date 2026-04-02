ff70dca2cb38c4a0bc73c155043e55d6310cc73d
davidtoussaintt <david.tou67@gmail.com>
2026-04-01 20:03:49 -0400

    ,

---
+++ exercices/module 4/4a/q2.py
@@ -0,0 +1,4 @@
+nom = input("Ecris ton nom: ")
+age = int(input("Ecris votre age: "))
+
+print(nom, "dans 5 ans tu auras", age+5, "ans")
---
+++ exercices/module 4/4a/q3.py
@@ -0,0 +1,5 @@
+n = float(input("Donnez moi valeur_1: "))
+z = float(input("Donnez moi valeur_2: "))
+
+print("Le produit des deux valeurs est", n*z)
+print("La difference des deux valeurs est", abs(n-z))
