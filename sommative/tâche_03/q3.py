e60a70f1fc60dfb78618185517524edb2d471663
davidtoussaintt <david.tou67@gmail.com>
2026-04-27 18:29:22 -0400

    .

---
+++ sommative/tâche_03/q3.py
@@ -0,0 +1,15 @@
+liste_a = ["Batterie", "Basse", "Piano", "Basse", "Guitare", "Batterie"]
+liste_b = ["Piano", "Voix", "Guitare", "Synthé", "Piano"]
+
+ensemble_a = set(liste_a)
+ensemble_b = set(liste_b)
+
+print("Chanson uniques du liste A sont", ensemble_a)
+print("Chanson uniques du liste A sont", ensemble_b)
+
+print("Chansons dans les deux listes :", ensemble_a & ensemble_b)
+
+print("Chansons seulement dans une des deux listes :", ensemble_a ^ ensemble_b)
+
+print("Toutes les chansons uniques combinées :", ensemble_a | ensemble_b)
+
